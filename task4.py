4214209ffaa238a9b9ae92df9ea32a0dd1f6d571
Przemyslaw Grzesiak <przemek.grzesiak91@gmail.com>
2021-12-12 15:33:35 +0100

    task4 added

---
+++ task4.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+chromosomes = list(range(1,22))
+chromosomes.append('X')
+
+for chr in chromosomes:
+    name = 'chr' + str(chr) + '.vcf'
+    chr_ins_tab = []
+    chr_del_tab = []
+    with open(name) as vcf:
+        for line in vcf:
+            if line[0:1] != '#':
+                ref = len(line.split('\t')[3])
+                alt = len(line.split('\t')[4])
+                diff = alt - ref
+
+                if diff > 0:
+                    chr_ins_tab.append(diff)
+                elif alt - ref <0:
+                    diff = abs(diff)
+                    chr_del_tab.append(diff)
+    #Insertions
+    plt.hist(chr_ins_tab,bins=100)
+    plt.title('Chr ' + str(chr) + ' insertions lengths')
+    name = 'Plots\\'+'chr'+str(chr)+'ins'
+    plt.savefig(name)
+    #Deletions
+    plt.hist(chr_del_tab,bins=100)
+    plt.title('Chr ' + str(chr) + ' deletions lengths')
+    name = 'Plots\\'+'chr'+str(chr)+'del'
+    plt.savefig(name)
+
